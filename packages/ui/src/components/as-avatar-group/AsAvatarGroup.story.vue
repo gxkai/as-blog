cdc6e0cf6ff6dfac1663495ca65e1b2ad74d9028
guxukai <gxk@33.cn>
2022-12-31 19:40:25 +0800

    chore: add checkbox;

---
+++ packages/ui/src/components/as-avatar-group/AsAvatarGroup.story.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import AsAvatarGroup from './AsAvatarGroup.vue'
+
+function initState() {
+  return {}
+}
+
+</script>
+<template>
+  <Story
+    :title="AsAvatarGroup.name"
+    :layout="{
+      type: 'grid',
+      width: 200,
+    }"
+  >
+    <Variant title="playground" :init-state="initState">
+      <template #default="{ state }">
+        <AsAvatarGroup v-bind="state" />
+      </template>
+    </Variant>
+  </Story>
+</template>
---
+++ packages/ui/src/components/as-checkbox/AsCheckbox.story.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import AsCheckbox from './AsCheckbox.vue'
+
+function initState() {
+  return {
+    src: '/raspberry.jpg'
+  }
+}
+
+</script>
+<template>
+  <Story
+    :title="AsCheckbox.name"
+    :layout="{
+      type: 'grid',
+      width: 200,
+    }"
+  >
+    <Variant title="playground" :init-state="initState">
+      <template #default="{ state }">
+        <AsCheckbox v-bind="state" />
+      </template>
+    </Variant>
+  </Story>
+</template>
